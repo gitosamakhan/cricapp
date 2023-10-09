4f249134c06ac7d1261fbc9539b55586cfe33f8e
Osama Khan <osama.khan@contour-software.com>
2023-10-09 22:16:09 +0500

    initial commit

---
+++ src/main/java/com/osama/cricapp/CricappApplication.java
@@ -0,0 +1,13 @@
+package com.osama.cricapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricappApplication.class, args);
+	}
+
+}
